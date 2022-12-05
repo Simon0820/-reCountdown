9e56ad77fcdf4695b4e5a069bf79038bbd344777
Simon Garding <91874587+Simon0820@users.noreply.github.com>
2022-12-05 14:37:58 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,3 +1,25 @@
 function hello(){
   console.log("hello");
 }
+
+var targetDate = new Date("Jan 5, 2024 15:37:25").getTime();
+
+var x = setInterval(function() {
+
+  var justNow = new Date().getTime();
+
+  var timeBetween = targetDate - justNow;
+
+  var days = Math.floor(timeBetween / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((timeBetween % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((timeBetween % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((timeBetween % (1000 * 60)) / 1000);
+
+  document.getElementById("clack").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  if (timeBetween < 0) {
+    clearInterval(x);
+    document.getElementById("clack").innerHTML = "Currently driving to big mountains in Åre";
+  }
+}, 1000);
